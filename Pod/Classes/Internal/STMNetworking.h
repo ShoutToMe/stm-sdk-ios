cb996566cd800ca5caf40a89ddc5e3527c078f1d
Tracy Rojas <tcrojas@gmail.com>
2017-08-10 16:48:09 -0700

    Refactor shout post to be able to work in the background

--- Pod/Classes/Internal/STMNetworking.h
+++ Pod/Classes/Internal/STMNetworking.h
@@ -14,8 +14,24 @@
 
 @end
 
-@interface STMUploadRequest : NSObject
+@protocol STMBackgroundSession <NSObject>
+
++ (void)setBackgroundCompletionHandler:(void (^)())completionHandler;
+
+@end
+
+@interface STMNetworking : NSObject
+
++ (void)handleEventsForBackgroundURLSession:(NSString *)identifier completionHandler:(void (^)())completionHandler;
+
+@end
+
+@interface STMUploadRequest : NSObject <NSURLSessionDataDelegate, STMBackgroundSession>
+
+@property id<STMUploadResponseHandlerDelegate> delegate;
 
 - (void)send:(NSDictionary *)data toUrl:(NSURL *)url usingHTTPMethod:(NSString *)httpMethod responseHandlerDelegate:(id<STMUploadResponseHandlerDelegate>)responseHandlerDelegate withCompletionHandler:(void (^)(NSError *, id))completionHandler;
+
+- (void)sendInBackground:(NSDictionary *)data toUrl:(NSURL *)url usingHTTPMethod:(NSString *)httpMethod responseType:(NSString *)responseType delegate:(id<STMUploadResponseHandlerDelegate>)delegate;
 
 @end
